e98e8167c36964fe1194d7af59faea7ccc6eede7
Mr Hardy <94104590+mrhardy23@users.noreply.github.com>
2023-03-30 11:11:39 +0100

    Uploaded the basic files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let openbutton = document.getElementById('menu-toggle');
+let closebutton = document.getElementById('closeicon');
+let panel = document.getElementById('mobilepanel');
+
+function closemobilepanel(){
+    panel.style.display="none";
+}
+
+function openmobilepanel(){
+    panel.style.display="block";
+}
